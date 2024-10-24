7525852bfd2463162cf52feadc86c24b4fb78c23
Kavya K S <73350000+kavyaks20@users.noreply.github.com>
2024-10-24 20:56:26 +0530

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,9 @@
+let a = document.getElementById("more")
+
+function  showMore() {
+    if (a.style.display === "none") {
+        a.style.display = "block";
+        } else {
+            a.style.display = "none";
+        }
+    }
